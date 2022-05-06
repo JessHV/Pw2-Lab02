bdb16e885ec08f39a613caa692929e34502b38ef
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-05-05 19:12:09 -0500

    Modifique como mostraba el codigo e hice que ignorara los espacios

--- Ejercicio4/ejercicio4.js
+++ Ejercicio4/ejercicio4.js
@@ -1,9 +1,13 @@
 function ingresar(){
     let link = document.getElementById("link").value;
+    link = link.replace(/\s/g,"")
 
     if(link.indexOf("https://meet.google.com/")!=-1){
-        link = link.replace(/https:\/\/meet.google.com\//g,"");
-        link = link.replace(/-/g,"");
-        document.getElementById("Codigo").innerHTML = "<h1>"+link+"</h1>";
+        let codigo = link.replace(/https:\/\/meet.google.com\//g,"");
+        codigo = codigo.replace(/-/g,"");
+        document.getElementById("Codigo").innerHTML = `
+        <h4>
+            Codigo sin guiones: ${codigo}
+        </h1>`;
     }
 }
