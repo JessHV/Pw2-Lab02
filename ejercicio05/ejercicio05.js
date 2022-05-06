931d7fbb6242791e6f0807b33d9d44bb45cf44b8
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-05 20:00:30 -0500

    Se creó el archivo ejercicio05.js con la función createTable()

--- ejercicio05/ejercicio05.js
+++ ejercicio05/ejercicio05.js
@@ -1,1 +1,19 @@
 // Script del ejercicio05
+
+var tableSize = 0;
+
+function createTable(){
+	tableSize = document.getElementById("tableSize").value;
+	let table = document.getElementById("valuesTable");
+
+	let tableInnerHTML = "";
+	tableInnerHTML += "<table>";
+	for(let i = 0; i < tableSize; i++){
+		tableInnerHTML += "<tr><td><input type='number' step='0.01' value='0'></td></tr>\n";
+	}
+	tableInnerHTML += "</table>";
+	table.innerHTML = tableInnerHTML;
+
+	document.getElementById("divCreateTable").setAttribute("style","display: none");
+	document.getElementById("divValuesTable").setAttribute("style","display: block");
+}
