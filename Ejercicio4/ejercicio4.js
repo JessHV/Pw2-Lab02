260cc1d5f4f42f4f129294652bba80b90894174d
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-05-05 19:00:20 -0500

    Hice una parte de la funcion ingresar, donde esta verificara que el link introducido sea valido

--- Ejercicio4/ejercicio4.js
+++ Ejercicio4/ejercicio4.js
@@ -1,3 +1,7 @@
 function ingresar(){
-    
+    let link = document.getElementById("link").value;
+
+    if(link.indexOf("https://meet.google.com/")!=-1){
+        document.getElementById("Codigo").innerHTML = "<h1>Funciona</h1>"
+    }
 }
