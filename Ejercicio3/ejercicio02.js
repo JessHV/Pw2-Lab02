72b8e6fa5b941fe2761ff725ce61a5a326823e90
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-05 23:12:46 -0500

    Hice un bucle en la funcion el cual devuelve los valores a la inversa

--- Ejercicio3/ejercicio02.js
+++ Ejercicio3/ejercicio02.js
@@ -1,4 +1,10 @@
 function reverseText(){
     let newText = document.getElementById("ReverseText");
     let text = document.getElementById("text").value;
+    let invText = "";
+    invText += "<label>";
+    for(let i=text.length; i > 0; i-- ){
+        invText += text.substring(i-1,i);
+    }
+    invText += "</label>";
 }
