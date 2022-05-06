801da31653da9422aeee1ffdf84386909f6a4704
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-05 20:33:35 -0500

    Funciones calcSum() que muestra la suma total dinamicamente, y getValuesFromTable() que retorna los valores ingresados en la tabla en un arreglo

--- ejercicio05/ejercicio05.js
+++ ejercicio05/ejercicio05.js
@@ -17,3 +17,24 @@ function createTable(){
 	document.getElementById("divCreateTable").setAttribute("style","display: none");
 	document.getElementById("divValuesTable").setAttribute("style","display: block");
 }
+
+function calcSum(){
+	let values = getValuesFromTable();
+	let sum = 0;
+	for(value of values){
+		sum += value;
+	}
+	document.getElementById("totalSum").innerHTML = sum;
+
+	document.getElementById("divTotalSum").setAttribute("style","display: block");
+}
+function getValuesFromTable(){
+	let values = [];
+	let tdTagValues = document.getElementsByTagName("td");
+	for(tag of tdTagValues){
+		let inputTag = tag.firstChild;
+		let val = parseFloat(inputTag.value);
+		values.push(val);
+	}
+	return values;
+}
